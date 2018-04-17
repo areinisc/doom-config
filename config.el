100a72d23f9ed2e51dced36ea18bb9ece5850071
Alex Reinisch <alex.p.reinisch@gmail.com>
2018-04-17 12:13:31 -0500

    Add automatic theme switching based on ambient light sensor
    See my gist for info:
    https://gist.github.com/areinisc/26560a375a397f6085f2228c9411d1d2
    Adding automatic light/dark theme switching to doom-emacs running on a
    mac with ambient light sensor.
    Idea adapted from: Matt Bilyeu
    <https://matthewbilyeu.com/blog/2018-04-09/setting-emacs-theme-based-on-ambient-light>
    
    Installation
    1. Make the lmutracker executable using `clang` and place it in your
    custom doom directory:
        ```
            $ cd ~/.doom.d/    # start in your custom doom directory, wherever that is for you.
            $ touch lmutracker.mm # copy in the contents of this file however you like
            $ clang -o lmutracker lmutracker.mm -framework IOKit -framework CoreFoundation  # this makes the executable
         ```
    2. Add the relevant elisp to your doom config (e.g.
    "~/.doom.d/config.el")
    3. Restart emacs and watch the theme automatically adjust to ambient
    light (Default settings will start checking light 10sec after emacs
    launches and recheck every 10sec thereafter.)

--- config.el
+++ config.el
@@ -2,8 +2,38 @@
 
 ;;; THEME
 
-;; Set theme to doom-nova (from doom-themes)
-(setq doom-theme 'doom-nova)
+;; When not on mac, set theme to doom-nova (from doom-themes)
+(unless (eq system-type 'darwin)
+  (setq doom-theme 'doom-nova))
+
+;; When on mac, use brightness sensor for theme adjustment
+(when (eq system-type 'darwin)
+  ;; Set your light and dark theme choices here!
+  (defconst light-theme 'doom-solarized-light)
+  (defconst dark-theme 'doom-nova)
+
+  (setq doom-theme 'doom-nova ; starting (dark) theme
+        theme-is-dark t)      ; boolean to track if theme is dark
+
+  ;; Set 2nd argument to how often (in seconds) you want to check light sensor.
+  (run-with-timer 10 10 #'change-theme-for-lighting))
+
+(defun change-theme-for-lighting ()
+  "Function for sensing light and changing themes."
+  (let* ((current-light-sensor-reading
+          (string-to-number
+           (shell-command-to-string
+            ;; this assumes you put the `lmutracker` executable
+            ;; in your doom-config directory ("~/.doom.d/" or "~/.config/doom/")
+            (concat doom-private-dir "lmutracker")))))
+    (if (< current-light-sensor-reading 100000) ; test if environment is low-light
+        (unless theme-is-dark         ; if theme is not yet dark
+          (load-theme dark-theme 1)   ; load dark theme
+          (setq theme-is-dark t))     ; note that theme is dark now
+
+      (when theme-is-dark             ; if theme is dark
+        (load-theme light-theme 1)    ; load light theme
+        (setq theme-is-dark nil)))))  ; note that theme is NOT dark now
 
 
 ;;; PLUGINS
