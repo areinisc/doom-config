8b93ba66ea49fae3e5660ac33dc7a1e8387034d5
Alex Reinisch <alex.p.reinisch@gmail.com>
2018-08-21 11:20:00 -0500

    Add LaTeXmk flag to LaTeX module call.

--- init.el
+++ init.el
@@ -97,7 +97,8 @@
       ;(java +meghanada) ; the poster child for carpal tunnel syndrome
       ;javascript        ; all(hope(abandon(ye(who(enter(here))))))
        julia             ; a better, faster MATLAB
-       latex             ; writing papers in Emacs has never been so fun
+       (latex            ; writing papers in Emacs has never been so fun
+        +latexmk)        ; compile with LaTeXmk instead
       ;ledger            ; an accounting system in Emacs
       ;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
