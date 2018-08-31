e56525740c8907a86a0c2449435d8c75e3f4496d
Alex Reinisch <alex.p.reinisch@gmail.com>
2018-08-31 08:55:50 -0500

    Patch emacs w/ Henrik's patch & some custom work.

--- packages.el
+++ packages.el
@@ -1,5 +1,12 @@
 ;;;  -*- lexical-binding: t; -*-
 
+;; Trying Henrik's macos patch.
+(package! exec-path-from-shell :disable t)
+;; have to edit /usr/local/Cellar/emacs-plus/Emacs.app/Contents/MacOS/Emacs
+;; change shebang line from #!/usr/bin/env bash to #!/usr/local/bin/bash
+;; Actually we're changing to #!/bin/bash and also adding -l to exec like so:
+;; exec "$SHELL" -l -c "$pwd/RunEmacs $args"
+
 ;; And paredit somehow already exists.
 ;; Let's get paredit up in here.
 ; (package! paredit)
